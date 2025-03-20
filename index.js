40d9e72361a373f67c1c6523433d679e63e70639
Yamila Martinez <yamila741@gmail.com>
2025-03-20 16:56:22 -0300

    reac App desplegada

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
@@ -16,15 +18,20 @@ dbConnection();
 app.use(cors());
 
 //Directorio publico
-app.use( express.static('public') );
+app.use(express.static('public'));
 
 //Lectura y parseo del body
-app.use( express.json() );
+app.use(express.json());
 
 
 //Rutas
-app.use('/api/auth', require('./routes/auth') );
-app.use('/api/events', require('./routes/events') );
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 //TODO: CRUD: Eventos
 
@@ -35,6 +42,6 @@ app.use('/api/events', require('./routes/events') );
 
 //Escuchar peticiones
 
-app.listen( process.env.PORT, () => {
-  console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
-} )
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+})
